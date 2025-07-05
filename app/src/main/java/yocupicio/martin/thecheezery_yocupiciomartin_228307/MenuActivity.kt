ae069ed9211f5e6f0a41ddc7fd82e0fc092cb6aa
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2025-07-05 09:53:15 -0700

    Proyecto restaurado. Se encontró el proyecto sin errores y terminado

--- app/src/main/java/yocupicio/martin/thecheezery_yocupiciomartin_228307/MenuActivity.kt
+++ app/src/main/java/yocupicio/martin/thecheezery_yocupiciomartin_228307/MenuActivity.kt
@@ -1,6 +1,8 @@
 package yocupicio.martin.thecheezery_yocupiciomartin_228307
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,34 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnCold: Button = findViewById(R.id.button_cold_drinks)
+        val btnHot: Button = findViewById(R.id.button_hot_drinks)
+        val btnSweet: Button = findViewById(R.id.button_sweets)
+        val btnSaltie: Button = findViewById(R.id.button_salties)
+
+        btnCold.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "cold")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "hot")
+            startActivity(intent)
+        }
+
+        btnSweet.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "sweet")
+            startActivity(intent)
+        }
+
+        btnSaltie.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "saltie")
+            startActivity(intent)
         }
     }
 }
